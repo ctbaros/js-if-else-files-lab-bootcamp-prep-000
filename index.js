d9919d420a5b9f06ad454e57d3115f166ad62f8f
Christian Baros <ctbaros@gmail.com>
2019-03-28 17:12:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var name = "Joe"
 if (name==="Joe") {
   console.log ("The name is correct")
+}else{
+  console.log("nothing")
 }
 var height = "74"
 if (height==="74") {
   console.log("The height is correct")
+}else{
+  console.log("nothing")
 }
 
 
